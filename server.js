5663986d2840e9a8933ba22962b67cd31738e770
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-15 12:47:43 -0600

    updated files

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const User = require('./model/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-mongoose.connect('mongodb+srv://jpzl_12:<password>@login-app.jlwrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+mongoose.connect('mongodb+srv://jpzl_12:Vegeta_San69@login-app.jlwrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -14,24 +17,103 @@ const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
-app.post('/api/register', async(req, res) => {
-    console.log(req.body)
-    // Analysts
-    // Scripts reading databases
+// Client -> Server: Your client *somehow* has to authenticate who it is
+// Why -> Server is a central computer which you control
+// Client -> a computer which you do not control
 
+// 1. Client proves itself somehow on the secrete/data is NON CHANGEABLE (JWT)
+// 2. Client-Server share a secret (Cookie)
+
+app.post('/api/change-password', async (req, res) => {
+    const { token, newpassword: plainTextPassword } = req.body
+
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' })
+    }
+
+    if (plainTextPassword.length < 5) {
+        return res.json({ 
+            status: 'error', 
+            error: 'Password too small. Should be at least 6 characters.'
+        })
+    }
+
+    try {
+        const user = jwt.verify(token, JWT_SECRET)
+
+        const _id = user.id
+
+        const password = await bcrypt.hash(plainTextPassword, 10)
+
+        await User.updateOne(
+            { _id }, 
+            {
+                $set: { password }
+            }
+        )
+        res.json({ status: 'ok' })
+    } catch (error) {
+        res.json({ status: 'error', error: ';'})
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await User.findOne({ username }).lean()
+
+    if (!user) {
+        return res.json({ status: 'error', error: 'Invalid username/password' })
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+        // the username, password combination is successful
+
+        const token = jwt.sign(
+            { 
+                id: user.id, 
+                username: user.username 
+            }, 
+            JWT_SECRET
+        )
+
+        return res.json({ status: 'ok', data: token })
+    }
+
+    res.json({ status: 'error', data: 'Invalid username/password' })
+})
+
+app.post('/api/register', async(req, res) => {
     const { username, password: plainTextPassword } = req.body
+
+    if (!username || typeof username !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid username' })
+    }
+    
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' })
+    }
+
+    if (plainTextPassword.length < 5) {
+        return res.json({ 
+            status: 'error', 
+            error: 'Password too small. Should be at least 6 characters' 
+        })
+    }
 
     const password = await bcrypt.hash(plainTextPassword, 10)
 
     try {
-        const response = await User.creat({
+        const response = await User.create({
             username,
             password
         })
         console.log('User created successfully: ', response)
     } catch (error) {
-        console.log(error)
-        return res.json({ status: 'error' })
+        if (error.code == 11000) {
+            // duplicate key
+            return res.json({ status: 'error', error: 'Username already in use' })
+        }
+        throw error
     }
 
     // console.log(await bcrypt.hash(password, 10))
